adf8e9c1e9e8568ff76406abbc7aa9e6aa1bedeb
highintothesky <highintothesky@gmail.com>
2013-06-05 15:52:08 +0200

    Reader aangepast, pakt nu losse field uit csv

--- org/uva/predictions/Reader.java
+++ org/uva/predictions/Reader.java
@@ -6,6 +6,16 @@ import java.io.FileReader;
 import java.io.IOException;
 
 
+/**
+ * 
+ * @author Niels Backer
+ * Reads a CSV dialog log file, extracts timestamps, question_ids among others.
+ * 
+ * @param	textfile	the CSV file you want to extract data from
+ * @return				
+ *
+ */
+
 public class Reader {
 	// string for file location
 	public static void main(String args[]){
@@ -21,12 +31,19 @@ public class Reader {
 			String str = src.readLine();
 			while(str != null){
 				StringTokenizer st = new StringTokenizer(str,
-				",.\n\t");
+				",\n\t");
 				while (st.hasMoreTokens()) {
-					String word = st.nextToken();
-					System.out.printf("%s \n", word);
+					int observation_id = Integer.parseInt(st.nextToken());
+					int form_id = Integer.parseInt(st.nextToken());
+					Double timestamp = Double.parseDouble(st.nextToken());
+					int question_id = Integer.parseInt(st.nextToken());
+					String answer = st.nextToken();
+					Boolean is_necessary = Boolean.valueOf(st.nextToken());
+					
+					System.out.printf("observation: %d form number: %d timestamp: %f question id: %d answer: %s \n", 
+							observation_id, form_id, timestamp, question_id, answer);
 				}
-				System.out.println("volgende regel");
+				System.out.println("volgende vraag");
 				str = src.readLine();
 			}
 		} catch (IOException e) {
