e43d5dcd0c4cbea9e9c12917e15160d4606322c0
Jouke van der Maas <jouke.vandermaas@gmail.com>
2013-06-18 19:59:48 +0200

    Added results directory

--- src/com/beinformed/research/labs/dialogprogress/testing/PredictorTester.java
+++ src/com/beinformed/research/labs/dialogprogress/testing/PredictorTester.java
@@ -1,31 +1,30 @@
 package com.beinformed.research.labs.dialogprogress.testing;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.beinformed.research.labs.dialogprogress.*;
-import com.beinformed.research.labs.dialogprogress.testing.*;
-
 
 public class PredictorTester {
-	public static void main(String[] args) throws InterruptedException 	{
-		if(args.length < 2) {
-			System.out.println("Syntax:\njava PredictorTester <in> <out>\n<in>: input file containing data\n<out>: results of the predictors");
+	public static void main(String[] args) throws InterruptedException {
+		if (args.length < 2) {
+			System.out
+					.println("Syntax:\njava PredictorTester <in> <out>\n<in>: input file containing data\n<out>: results of the predictors");
 			System.exit(1);
 		}
-		
+
 		String dataLoc = args[0];
 		String outputLoc = args[1];
-		
+
 		List<Predictor> predictors = new ArrayList<Predictor>();
 		predictors.add(new BaseLinePredictor(PredictionUnit.Time));
 		predictors.add(new BaseLinePredictor(PredictionUnit.Steps));
-		predictors.add(new PerObservationBLPredictor(PredictionUnit.Time));	
+		predictors.add(new PerObservationBLPredictor(PredictionUnit.Time));
 		predictors.add(new PerObservationBLPredictor(PredictionUnit.Steps));
-		
+
 		TestFrame frame = new TestFrame(predictors);
-		
+
 		DataReader reader = null;
 		try {
 			reader = new DataReader(dataLoc);
@@ -39,20 +38,30 @@ public class PredictorTester {
 			System.exit(1);
 		}
 
-		
 		List<Observation> data = reader.getObservations();
-		
+
 		Iterable<TestResult> result = frame.testAll(data);
-		
+
 		try {
 			ResultWriter writer = new ResultWriter(outputLoc);
 			writer.write(result);
 			writer.close();
 			System.out.println("Written results to " + outputLoc);
-			
+			Process python = new ProcessBuilder("python", "src/plot.py", outputLoc, "results/graph.png")
+					.start();
+
+			String line;
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					python.getInputStream()));
+			while ((line = input.readLine()) != null) {
+				System.out.println(line);
+			}
+			input.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
