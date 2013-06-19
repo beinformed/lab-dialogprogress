02527302fc645220c303afc4224d1adf15e5b99c
Jouke van der Maas <jouke.vandermaas@gmail.com>
2013-06-19 15:01:35 +0200

    Better datagen

--- src/com/beinformed/research/labs/dialogprogress/testing/DataGenerator.java
+++ src/com/beinformed/research/labs/dialogprogress/testing/DataGenerator.java
@@ -6,80 +6,16 @@ import java.util.*;
 
 public class DataGenerator {
 	
-	private class Form {
-		final int totalq;
-		final int askedmin;
-		final int askedmax;
-		private Map<Integer, Integer> qmin = new HashMap<Integer, Integer>();
-		private Map<Integer, Integer> qmax = new HashMap<Integer, Integer>();
-		
-		public Form() {
-			this.totalq = rand.nextInt(maxQuestionsPerForm - minQuestionsPerForm) + minQuestionsPerForm;
-			this.askedmin = rand.nextInt(totalq / 3);
-			this.askedmax = askedmin + rand.nextInt(totalq - askedmin) + 1;
-		}
-		
-		public int duration(int q) {
-			if(!qmin.containsKey(q))
-				generateQRange(q);
-			
-			return rand.nextInt(qmax.get(q) - qmin.get(q)) + qmin.get(q);
-		}
-
-		private void generateQRange(int q) {
-			qmin.put(q, rand.nextInt(300) + 10);
-			qmax.put(q, qmin.get(q) + rand.nextInt(qmin.get(q) * 2) + 1);
-		}
-	}
-	
-	/*
-	 * Form 1: 20 questions
-	 * * * * * * * * * * * * *
-	 * between 10 and 15 questions are asked. 
-	 * 
-	 * Form 2: 30 questions
-	 * between 10 and 20 questions are asked.
-	 */
 	private FileWriter writer;
-	private Random rand;	
-	private Form[] forms;
-	private int minQuestionsPerForm = 15;
-	private int maxQuestionsPerForm = 50;
-
+	
 	public DataGenerator(String path) throws IOException {
 		this.writer = new FileWriter(path);
-		this.rand = new Random();
-		this.forms = new Form[] { 
-				new Form(),
-				new Form(),
-				new Form(),
-				new Form()
-			};
 	}
 
-	public void generate(int n) throws IOException {	
+	public void generate(int n) throws IOException {
+		Form form = new Form();
 		for(int i = 0; i < n; i++) {
-			int formid = rand.nextInt(forms.length);
-			Form form = forms[formid];
-			List<Long> questions = new ArrayList<Long>();
-			long base = System.currentTimeMillis();
-			int noQuestions = form.askedmin + rand.nextInt(form.askedmax - form.askedmin);
-			for(int j = 0; j < noQuestions; j++) {
-				int q = rand.nextInt(form.totalq);
-				int time = form.duration(q);
-				base += time;
-				questions.add(base);
-			}
-			write(i, formid, questions);
-		}
-	}
-	
-	private void write(int observation, int formid, List<Long> questions) throws IOException {
-		String form = Integer.toString(formid);
-		String obs = Integer.toString(observation);
-		
-		for(Long l : questions) {
-			writer.write(form + "," + obs + "," + l.toString() + ",[DataAnchor [q]],DataAnchor [q]=a,OK\n");
+			writer.write(form.generateObservation());
 		}
 	}
 
